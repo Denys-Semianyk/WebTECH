bdc2c031f5e89a2dac0bff51cde043218f501a05
Denys-Semianyk <143899947+Denys-Semianyk@users.noreply.github.com>
2025-10-13 20:53:36 +0300

    Update script.js

--- lab3/script.js
+++ lab3/script.js
@@ -1,301 +1,239 @@
-var car1 = new Object();
-
-car1.color = "red"; // довільний колір англійською
-car1.maxSpeed = 90; // довільне ціле число
+const car1 = new Object();
+car1.color = "red";
+car1.maxSpeed = 90;
 car1.driver = {
-  name: "Сем'яник Денис", // твоє ім'я та прізвище
-  category: "C",
-  personalLimitations: "No driving at night"
+  name: "Сем'яник Денис",
+  category: "B",
+  personalLimitations: "no night driving",
 };
 car1.tuning = true;
 car1.numberOfAccidents = 0;
 
-var car2 = {
-    color: "blue", // довільний колір англійською
-    maxSpeed: 180, // довільне ціле число
-    driver: {
-      name: "Сем'яник Денис", // твоє ім'я та прізвище
-      category: "B",
-      personalLimitations: null
-    },
-    tuning: false,
-    numberOfAccidents: 2
-  };
-  
-  car1.drive = function() {
-    console.log("I am not driving at night");
-  };
-  
-  // Демонстрація роботи:
-  car1.drive();
+const car2 = {
+  color: "blue",
+  maxSpeed: 200,
+  driver: {
+    name: "Сем'яник Денис",
+    category: "B",
+    personalLimitations: null,
+  },
+  tuning: false,
+  numberOfAccidents: 2,
+};
 
-  car2.drive = function() {
-    console.log("I can drive anytime");
-  };
-  
-  // Демонстрація роботи:
-  car2.drive();
+car1.drive = function () {
+  console.log("I am not driving at night");
+};
 
-  function Truck(color, weight, avgSpeed, brand, model) {
-    this.color = color;
-    this.weight = weight;
-    this.avgSpeed = avgSpeed;
-    this.brand = brand;
-    this.model = model;
-  
-    // Метод trip (з 1.2.9) додамо сюди:
-    this.trip = function() {
-      if (!this.driver) {
-        console.log("No driver assigned");
+car2.drive = function () {
+  console.log("I can drive anytime");
+};
+
+car1.drive();
+car2.drive();
+
+function Truck(color, weight, avgSpeed, brand) {
+  this.color = color;
+  this.weight = weight;
+  this.avgSpeed = avgSpeed;
+  this.brand = brand;
+  this.driver = null;
+  this.trip = function () {
+    if (this.driver) {
+      if (this.driver.nightDriving) {
+        console.log(
+          `Driver ${this.driver.name} drives at night with truck ${this.brand}.`
+        );
       } else {
-        var msg = "Driver " + this.driver.name + " ";
-        msg += this.driver.nightDriving ? "drives at night " : "does not drive at night ";
-        msg += "and has " + this.driver.experience + " years of experience";
-        console.log(msg);
+        console.log(
+          `Driver ${this.driver.name} does not drive at night with truck ${this.brand}.`
+        );
       }
-    };
-  }
-
-  Truck.AssignDriver = function(truckObj, name, nightDriving, experience) {
-    truckObj.driver = {
-      name: name,
-      nightDriving: nightDriving,
-      experience: experience
-    };
+    } else {
+      console.log("No driver assigned.");
+    }
   };
+}
 
-var truck1 = new Truck("green", 5000, 80.5, "Renault", "Magnum");
-var truck2 = new Truck("yellow", 4500, 75.2, "Scania", "R500");
+Truck.AssignDriver = function (truck, driverName, nightDriving) {
+  truck.driver = { name: driverName, nightDriving: nightDriving };
+};
 
-// Присвоюємо водіїв
-Truck.AssignDriver(truck1, "Іван Іванов", true, 10);
-Truck.AssignDriver(truck2, "Петро Петров", false, 5);
+const truck1 = new Truck("yellow", 4000, 90, "MAN");
+const truck2 = new Truck("white", 3500, 80, "Volvo");
 
-// Демонстрація роботи методу trip:
+Truck.AssignDriver(truck1, "Сем'яник Денис", true);
+Truck.AssignDriver(truck2, "Сем'яник Денис", false);
+
 truck1.trip();
 truck2.trip();
 
-
 class Square {
-    constructor(a) {
-      this.a = a; // сторона квадрата
-    }
-  
-    static help() {
-      console.log("Square: all sides equal; 4 right angles; perimeter = 4 * a; area = a^2");
-    }
-  
-    length() {
-      console.log(`Perimeter: ${4 * this.a}`);
-    }
-  
-    square() {
-      console.log(`Area: ${this.a * this.a}`);
-    }
-  
-    info() {
-      console.log(`Square info:
+  constructor(a) {
+    this.a = a;
+  }
+
+  static help() {
+    console.log("Square: all sides equal; perimeter = 4a; area = a^2");
+  }
+
+  length() {
+    console.log(`Perimeter: ${4 * this.a}`);
+  }
+
+  square() {
+    console.log(`Area: ${this.a * this.a}`);
+  }
+
+  info() {
+    console.log(`Square info:
   - Sides: ${this.a}, ${this.a}, ${this.a}, ${this.a}
   - Angles: 90°, 90°, 90°, 90°
   - Perimeter: ${4 * this.a}
   - Area: ${this.a * this.a}`);
-    }
+  }
+}
+
+class Rectangle extends Square {
+  constructor(a, b) {
+    super(a);
+    this._a = a;
+    this._b = b;
   }
 
-  
-  class Rectangle extends Square {
-    constructor(a, b) {
-      super(a);
-      this.b = b; // ширина прямокутника
-    }
-  
-    static help() {
-      console.log("Rectangle: opposite sides equal; 4 right angles; perimeter = 2*(a+b); area = a*b");
-    }
-  
-    length() {
-      console.log(`Perimeter: ${2 * (this.a + this.b)}`);
-    }
-  
-    square() {
-      console.log(`Area: ${this.a * this.b}`);
-    }
-  
-    info() {
-      console.log(`Rectangle info:
+  get a() {
+    return this._a;
+  }
+
+  set a(value) {
+    this._a = value;
+  }
+
+  get b() {
+    return this._b;
+  }
+
+  set b(value) {
+    this._b = value;
+  }
+
+  static help() {
+    console.log(
+      "Rectangle: opposite sides equal; 4 right angles; perimeter = 2*(a+b); area = a*b"
+    );
+  }
+
+  length() {
+    console.log(`Perimeter: ${2 * (this.a + this.b)}`);
+  }
+
+  square() {
+    console.log(`Area: ${this.a * this.b}`);
+  }
+
+  info() {
+    console.log(`Rectangle info:
   - Sides: ${this.a}, ${this.b}, ${this.a}, ${this.b}
   - Angles: 90°, 90°, 90°, 90°
   - Perimeter: ${2 * (this.a + this.b)}
   - Area: ${this.a * this.b}`);
-    }
+  }
+}
+
+class Rhombus extends Square {
+  constructor(a, alpha, beta) {
+    super(a);
+    this.alpha = alpha;
+    this.beta = beta;
   }
 
-  
-  class Rhombus extends Square {
-    constructor(a, alpha, beta) {
-      super(a);
-      this.alpha = alpha; // тупий кут
-      this.beta = beta;   // гострий кут
-    }
-  
-    static help() {
-      console.log("Rhombus: all sides equal; opposite angles equal; perimeter = 4 * a; area = a^2 * sin(alpha)");
-    }
-  
-    length() {
-      console.log(`Perimeter: ${4 * this.a}`);
-    }
-  
-    square() {
-      const radians = (deg) => (deg * Math.PI) / 180;
-      console.log(`Area: ${(this.a ** 2) * Math.sin(radians(this.alpha))}`);
-    }
-  
-    info() {
-      console.log(`Rhombus info:
+  static help() {
+    console.log("Rhombus: all sides equal; opposite angles equal");
+  }
+
+  length() {
+    console.log(`Perimeter: ${4 * this.a}`);
+  }
+
+  square() {
+    console.log(`Area: ${this.a ** 2 * Math.sin((this.alpha * Math.PI) / 180)}`);
+  }
+
+  info() {
+    console.log(`Rhombus info:
   - Sides: ${this.a}, ${this.a}, ${this.a}, ${this.a}
   - Angles: ${this.alpha}°, ${this.beta}°, ${this.alpha}°, ${this.beta}°
   - Perimeter: ${4 * this.a}
-  - Area: ${(this.a ** 2) * Math.sin((this.alpha * Math.PI) / 180)}`);
-    }
+  - Area: ${(this.a ** 2 * Math.sin((this.alpha * Math.PI) / 180)).toFixed(2)}`);
+  }
+}
+
+class Parallelogram extends Rhombus {
+  constructor(a, b, alpha, beta) {
+    super(a, alpha, beta);
+    this.b = b;
   }
 
-  
+  static help() {
+    console.log("Parallelogram: opposite sides equal, opposite angles equal");
+  }
 
-  class Parallelogram extends Rhombus {
-    constructor(a, b, alpha, beta) {
-      super(a, alpha, beta);
-      this.b = b;
-    }
-  
-    static help() {
-      console.log("Parallelogram: opposite sides equal; opposite angles equal; perimeter = 2*(a+b); area = a*b*sin(alpha)");
-    }
-  
-    length() {
-      console.log(`Perimeter: ${2 * (this.a + this.b)}`);
-    }
-  
-    square() {
-      const radians = (deg) => (deg * Math.PI) / 180;
-      console.log(`Area: ${this.a * this.b * Math.sin(radians(this.alpha))}`);
-    }
-  
-    info() {
-      console.log(`Parallelogram info:
+  length() {
+    console.log(`Perimeter: ${2 * (this.a + this.b)}`);
+  }
+
+  square() {
+    console.log(`Area: ${this.a * this.b * Math.sin((this.alpha * Math.PI) / 180)}`);
+  }
+
+  info() {
+    console.log(`Parallelogram info:
   - Sides: ${this.a}, ${this.b}, ${this.a}, ${this.b}
   - Angles: ${this.alpha}°, ${this.beta}°, ${this.alpha}°, ${this.beta}°
   - Perimeter: ${2 * (this.a + this.b)}
-  - Area: ${this.a * this.b * Math.sin((this.alpha * Math.PI) / 180)}`);
-    }
+  - Area: ${(this.a * this.b * Math.sin((this.alpha * Math.PI) / 180)).toFixed(2)}`);
   }
-
-  
-  class RectangleWithAccessors extends Square {
-    constructor(a, b) {
-      super(a);
-      this._b = b;
-    }
-  
-    get a() {
-      return this._a;
-    }
-  
-    set a(value) {
-      this._a = value;
-    }
-  
-    get b() {
-      return this._b;
-    }
-  
-    set b(value) {
-      this._b = value;
-    }
-  
-    static help() {
-      console.log("Rectangle with getters/setters: opposite sides equal; 4 right angles; perimeter = 2*(a+b); area = a*b");
-    }
-  
-    length() {
-      console.log(`Perimeter: ${2 * (this.a + this.b)}`);
-    }
-  
-    square() {
-      console.log(`Area: ${this.a * this.b}`);
-    }
-  
-    info() {
-      console.log(`Rectangle info:
-  - Sides: ${this.a}, ${this.b}, ${this.a}, ${this.b}
-  - Angles: 90°, 90°, 90°, 90°
-  - Perimeter: ${2 * (this.a + this.b)}
-  - Area: ${this.a * this.b}`);
-    }
-  }
-
-  
-Square.help();
-Rectangle.help();
-Rhombus.help();
-Parallelogram.help();
-
-const sq = new Square(5);
-const rect = new Rectangle(6, 3);
-const rhomb = new Rhombus(4, 120, 60);
-const para = new Parallelogram(7, 4, 110, 70);
-
-sq.info();
-rect.info();
-rhomb.info();
-para.info();
+}
 
 function Triangular(a = 3, b = 4, c = 5) {
-    return { a, b, c };
-  }
+  this.a = a;
+  this.b = b;
+  this.c = c;
+}
 
-  const triangle1 = Triangular(); // за замовчуванням (3, 4, 5)
-  const triangle2 = Triangular(6, 8, 10); // інші значення
-  const triangle3 = Triangular(5, 12, 13); // інші значення
-  
-  console.log(triangle1);
-  console.log(triangle2);
-  console.log(triangle3);
+const t1 = new Triangular();
+const t2 = new Triangular(6, 8, 10);
+const t3 = new Triangular(5, 5, 8);
+console.log("Triangular objects:");
+console.log(`t1: a=${t1.a}, b=${t1.b}, c=${t1.c}`);
+console.log(`t2: a=${t2.a}, b=${t2.b}, c=${t2.c}`);
+console.log(`t3: a=${t3.a}, b=${t3.b}, c=${t3.c}`);
 
-  function PiMultiplier(multiplier) {
-    return function() {
-      return Math.PI * multiplier;
-    };
-  }
+function PiMultiplier(k) {
+  this.k = k;
+  this.multiply = function () {
+    return Math.PI * this.k;
+  };
+}
 
-const multiplyPiBy2 = PiMultiplier(2);
-const multiplyPiBy3 = PiMultiplier(3);
-const dividePiBy2 = PiMultiplier(0.5);
+const pm1 = new PiMultiplier(2);
+const pm2 = new PiMultiplier(3);
+const pm3 = new PiMultiplier(0.5);
+console.log(pm1.multiply(), pm2.multiply(), pm3.multiply());
 
-console.log(multiplyPiBy2()); // Множить на 2
-console.log(multiplyPiBy3()); // Множить на 3
-console.log(dividePiBy2());   // Ділить на 2
+function Painter(name) {
+  this.name = name;
+  this.paint = function (obj) {
+    if (!obj.type) {
+      console.log(`Painter ${this.name} cannot paint: object has no 'type' property.`);
+    } else {
+      console.log(`Painter ${this.name} paints a ${obj.type}.`);
+    }
+  };
+}
 
-
-function Painter(color) {
-    return function(obj) {
-      if (obj.hasOwnProperty('type')) {
-        console.log(`${color}: ${obj.type}`);
-      } else {
-        console.log("No 'type' property occurred!");
-      }
-    };
-  }
-
-const PaintBlue = Painter("Blue");
-const PaintRed = Painter("Red");
-const PaintYellow = Painter("Yellow");
-
-const obj1 = { maxSpeed: 280, type: "Truck", loadCapacity: 2400 };
-const obj2 = { maxSpeed: 180, type: "Sportcar", color: "magenta" };
-const obj3 = { avgSpeed: 90, isCar: true, color: "purple" };
-
-// Демонстрація роботи функцій
-PaintBlue(obj1);   // Blue: Truck
-PaintRed(obj2);    // Red: Sportcar
-PaintYellow(obj3); // No 'type' property occurred!
+const painter1 = new Painter("Leo");
+const obj1 = { type: "car" };
+const obj2 = { name: "something" };
+painter1.paint(obj1);
+painter1.paint(obj2);
